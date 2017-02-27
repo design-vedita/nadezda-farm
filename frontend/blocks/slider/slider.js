bfc2df8e9c34e1c6a902dd090f862c9fc492991b
design-vedita <design-vedita@yandex.ru>
2017-02-27 18:04:31 +0300

    index page header, slider 70%, action-good 50%

---
+++ frontend/blocks/slider/slider.js
@@ -0,0 +1,28 @@
+import Module from '../../includes/Module';
+import Swiper from '../../../node_modules/swiper/dist/js/swiper.min';
+
+export default class SliderIndex extends Module {
+
+    constructor(){
+        super({
+            name: 'SliderIndex',
+            self: '.js-slider'
+        });
+    }
+
+    init() {
+        this.initSlider();
+    }
+
+    initSlider() {
+        this.$slider = new Swiper(this.$root, {
+            paginationClickable: true,
+            loop: true,
+            slidesPerView: 1,
+            autoplay: 2500,
+            autoplayDisableOnInteraction: false,
+            nextButton: '.js-slider-next',
+            prevButton: '.js-slider-prev'
+        });
+    }
+}
